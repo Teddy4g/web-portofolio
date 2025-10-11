49ef3bf21015e38f39cc241b15c580488b5c733d
teddy <teddyagustinus56588@gmail.com>
2025-10-11 13:16:41 +0700

    Modify About Section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -26,10 +26,7 @@ export const AboutSection = () => {
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
-              <a href="#contact" className="cosmic-button">
-                {" "}
-                Get In Touch
-              </a>
+            
 
               <a
                 href="https://www.linkedin.com/in/teddyagustinus/"
